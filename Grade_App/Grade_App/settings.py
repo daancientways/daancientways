47ddd85375d3f65f77163a2286a115731a58d60f
Ways <daanarchist22@gmail.com>
2026-03-26 10:52:48 +0200

    Update ptoject files

--- Grade_App/Grade_App/settings.py
+++ Grade_App/Grade_App/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Grade_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'grade_appdb',
+        'USER' : 'root',
+        'PASSWORD' : 'Anarchist#22',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
 
